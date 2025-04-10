d2f15f01b4f6064c6b8ef7f818533d370817d6b0
MSI\DeepWise-DeanWang <dean10517@gmail.com>
2025-04-11 00:57:04 +0800

    [再修正]

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,12 +1,13 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
   content: [
-    "./src/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {},
   },
   plugins: [],
-}
-export default config
+};
+
+export default config;
